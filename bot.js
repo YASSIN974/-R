072b818fd153ea68d68403ba24b86b5768bb03a5
YASSIN974 <44032576+YASSIN974@users.noreply.github.com>
2018-11-01 18:15:38 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js')
+const client = new Discord.Client();
+client.on('ready', function(){
+ console.log(`Logged in as ${client.user.tag}!`);
+    
+   // var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293'];
+   var s = ['483055660209012736','480169573530861578','483055655800930315'];
+    setInterval(function (){
+    client.user.setPresence({
+ game: { 
+    type: 1,
+     url: 'https://www.twitch.tv/skwadraa',
+    name: 'I AM BAD',
+    application_id: '477187715658547201',
+     assets: {
+         large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
+  
+    }
+  }
+    });
+    }, 5000);
+});
+client.login(process.env.BOT_TOKEN);
