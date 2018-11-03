a53be1f12f8cdd98af6a6728497c787dbbf72f14
YASSIN974 <44032576+YASSIN974@users.noreply.github.com>
2018-11-03 07:39:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', function(){
  game: { 
     type: 1,
      url: 'https://www.twitch.tv/skwadraa',
-    name: 'TRASH LIFE',
+    name: 'I LOVE ONE ',
     application_id: '477187715658547201',
      assets: {
          large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
