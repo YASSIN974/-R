7c042268cea8a0db7ebd3d32652075be266b73a3
YASSIN974 <44032576+YASSIN974@users.noreply.github.com>
2018-11-03 07:57:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', function(){
  game: { 
     type: 1,
      url: 'https://www.twitch.tv/skwadraa',
-    name: 'I LOVE ONE ',
+    name: 'I HATE ALL ',
     application_id: '477187715658547201',
      assets: {
          large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
